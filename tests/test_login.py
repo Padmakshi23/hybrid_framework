10747eb1db2e4c6ac97c999dc3931b4f3a993892
padmakshi <padmakshi231@gmail.com>
2023-03-02 12:09:09 +0530

    Framework: Add login page

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from assertpy import assert_that
+from selenium.webdriver.common.by import By
+
 
 class TestLoginUI:
     def test_title(self):
@@ -11,3 +13,12 @@ class TestLoginUI:
         #assert actual_title == "OrangeHRM"
         #assert "HRM" in actual_title
         assert_that("OrangeHRM").is_equal_to(actual_title)
+
+    def test_header(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(20)
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        actual_header = driver.find_element(By.XPATH, "//h5[text()='Login']").text
+        assert_that("Login").is_equal_to(actual_header)
+
