efde6b4ab141658ce1c9de73a99ef06e96e747b1
padmakshi <padmakshi231@gmail.com>
2023-03-06 11:13:25 +0530

    Framework:Valid_login

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,8 +4,27 @@ from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 
+class TestLogin:
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
+    def test_valid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
+        assert_that("Dashboard").is_equal_to(actual_text)
+
+
 class TestLoginUI:
-    @pytest.fixture(scope="function",autouse=True)
+    @pytest.fixture(scope="function", autouse=True)
     def setup(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
