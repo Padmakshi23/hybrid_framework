f015ef01e2487dab85fb371ab8fe382c43fbaabb
padmakshi <padmakshi231@gmail.com>
2023-03-16 11:34:18 +0530

    updated work on json

--- Base/webdriver_listener.py
+++ Base/webdriver_listener.py
@@ -1,12 +1,22 @@
 import pytest
 from selenium import webdriver
 
+from Uitilites import read_utilites
+
 
 class WebDriverWrapper:
     driver = None
 
     @pytest.fixture(scope="function", autouse=True)
     def browser_config(self):
+        browser_name = read_utilites.get_value_from_json("../test_data/data.json", "browser")
+
+        if browser_name == "edge":
+            self.driver = webdriver.Edge()
+        elif browser_name == "ff":
+            self.driver = webdriver.Firefox()
+        else:
+            self.driver = webdriver.Chrome()
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
