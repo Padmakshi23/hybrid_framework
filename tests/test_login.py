92c14dc12eacb380b26f2b284ccc43e6776ba439
padmakshi <padmakshi231@gmail.com>
2023-03-17 11:31:26 +0530

    Created Page object for place holder

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,27 +11,21 @@ from Base.webdriver_listener import WebDriverWrapper
 class TestLogin(WebDriverWrapper):
 
     def test_valid_login(self):
-        # self.driver.find_element(By.NAME, "username").send_keys("Admin")
-        # self.driver.find_element(By.NAME, "password").send_keys("admin123")
-        # self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
-
         login_page = LoginPage(self.driver)
         login_page.enter_username("Admin")
         login_page.enter_password("admin123")
         login_page.click_on_login()
 
-        dashboard_page = DashboardPage()
-        #actual_text = dashboard_page.get_dashboard_header()
+        dashboard_page = DashboardPage(self.driver)
         assert_that("Dashboard").is_equal_to(dashboard_page.get_dashboard_header)
 
     @pytest.mark.parametrize("username,password,expected_error", data_source.test_invalid_login_data)
     def test_invalid_login(self, username, password, expected_error):
-        self.driver.find_element(By.NAME, "username").send_keys(username)
-        self.driver.find_element(By.NAME, "password").send_keys(password)
-        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
-        actual_error = self.driver.find_element(By.XPATH, "//p[text()='Invalid credentials']").text
-        # print(actual_error)
-        assert_that(expected_error).is_equal_to(actual_error)
+        login_page = LoginPage(self.driver)
+        login_page.enter_username(username)
+        login_page.enter_password(password)
+        login_page.click_on_login()
+        assert_that(expected_error).is_equal_to(login_page.get_invalid_error_message)
 
 
 class TestLoginUI(WebDriverWrapper):
@@ -49,7 +43,6 @@ class TestLoginUI(WebDriverWrapper):
         print("check hrf")
 
     def test_login_placeholder(self):
-        actual_username_placeholder = self.driver.find_element(By.NAME, "username").get_attribute("placeholder")
-        assert_that("Username").is_equal_to(actual_username_placeholder)
-        actual_password_placeholder = self.driver.find_element(By.NAME, "password").get_attribute("placeholder")
-        assert_that("Password").is_equal_to(actual_password_placeholder)
+        login_page = LoginPage(self.driver)
+        assert_that("Username").is_equal_to(login_page.get_username_placeholder)
+        assert_that("Password").is_equal_to(login_page.get_password_placeholder)
